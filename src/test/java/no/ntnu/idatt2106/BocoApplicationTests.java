329215f84349a47c9f23477b62f9531b0272e07d
Erik Borgeteien Hansen <erik@erikbhan.no>
2022-04-21 14:09:15 +0200

    comment out unused import temproarily

--- src/test/java/no/ntnu/idatt2106/BocoApplicationTests.java
+++ src/test/java/no/ntnu/idatt2106/BocoApplicationTests.java
@@ -1,6 +1,6 @@
 package no.ntnu.idatt2106;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
