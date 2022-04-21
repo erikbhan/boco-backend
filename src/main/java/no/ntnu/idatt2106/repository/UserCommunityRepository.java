112ec2543cf9617ede9b6b782d58ecba9c8e1b83
henrikburmann <haburmann@gmail.com>
2022-04-21 16:02:59 +0200

    Merge JPADatabase attempt

--- src/main/java/no/ntnu/idatt2106/repository/UserCommunityRepository.java
+++ src/main/java/no/ntnu/idatt2106/repository/UserCommunityRepository.java
@@ -3,5 +3,5 @@ package no.ntnu.idatt2106.repository;
 import no.ntnu.idatt2106.model.DAO.UserCommunityDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserGroupRepository extends JpaRepository<UserCommunityDAO, Long> {
+public interface UserCommunityRepository extends JpaRepository<UserCommunityDAO, Long> {
 }
