1148986a6621b6fc5b1d2aa12e2f8c95e2404c7c
Aleksander halvorsen Holthe <alek-hh@hotmail.com>
2022-04-22 13:06:48 +0200

    Started working on HTTP status for controller

--- src/main/java/no/ntnu/idatt2106/controller/RentController.java
+++ src/main/java/no/ntnu/idatt2106/controller/RentController.java
@@ -1,7 +1,11 @@
 package no.ntnu.idatt2106.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import no.ntnu.idatt2106.exception.StatusCodeException;
 import no.ntnu.idatt2106.model.DTO.RentDTO;
 import no.ntnu.idatt2106.service.RentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@ApiResponse(responseCode = "200", description = "OK")
 public class RentController {
 
     private final RentService rentService;
@@ -17,9 +22,10 @@ public class RentController {
         this.rentService = rentService;
     }
 
+    @Operation(summary = "Adds new rent")
+    @ApiResponse(responseCode = "201", description = "Created")
     @PostMapping("/rent/add")
     public void addRent(@RequestBody RentDTO rentDTO) {
         rentService.saveRent(rentDTO);
     }
-
 }
