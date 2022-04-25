3799358fd41d008184f092a9846b76a8683a8b2a
Haakon Tideman Kanter <haakotka@stud.ntnu.no>
2022-04-25 09:30:16 +0200

    formatting

--- src/main/java/no/ntnu/idatt2106/controller/UserCommunityController.java
+++ src/main/java/no/ntnu/idatt2106/controller/UserCommunityController.java
@@ -25,28 +25,30 @@ public class UserCommunityController {
         this.userService = userService;
         this.communityRepository = communityRepository;
     }
+
     @Operation(summary = "Add user to a community")
     @PostMapping("/addUserToCommunity")
     @ApiResponse(responseCode = "200", description = "Added user to community")
     @ApiResponse(responseCode = "400", description = "Illegal operation")
     public void addUserToCommunity(@RequestBody UserCommunityDTO userCommunityDTO) throws StatusCodeException {
-        UserDAO user =  userService.findUserByUserId(userCommunityDTO.getUserID());
-        if (user==null){
+        UserDAO user = userService.findUserByUserId(userCommunityDTO.getUserID());
+        if (user == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "User does not exist");
         }
         CommunityDAO communityDAO = communityRepository.findCommunityDAOByCommunityID(userCommunityDTO.getCommunityID());
-        if(communityDAO==null){
+        if (communityDAO == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "Community does not exist");
         }
         userCommunityService.addUserToCommunity(user, communityDAO);
     }
+
     @Operation(summary = "Get all communities a specific user is part of")
     @ApiResponse(responseCode = "200", description = "Found communities")
     @ApiResponse(responseCode = "400", description = "Illegal operation")
     @GetMapping("/getCommunitiesForUser")
-    public void getCommunitiesForUser(@RequestBody UserCommunityDTO userCommunityDTO ) throws StatusCodeException {
+    public void getCommunitiesForUser(@RequestBody UserCommunityDTO userCommunityDTO) throws StatusCodeException {
         UserDAO user = userService.findUserByUserId(userCommunityDTO.getUserID());
-        if(user==null){
+        if (user == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "User does not exist");
         }
         userCommunityService.getAllCommunitiesForUser(user);
