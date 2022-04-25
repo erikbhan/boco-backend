ca0f90521a9012566318ce6543265b06fe0e9f40
Haakon Tideman Kanter <haakotka@stud.ntnu.no>
2022-04-25 13:47:27 +0200

    Used tokenUtil instead of UserCommunityDTO

--- src/main/java/no/ntnu/idatt2106/controller/UserCommunityController.java
+++ src/main/java/no/ntnu/idatt2106/controller/UserCommunityController.java
@@ -3,21 +3,24 @@ package no.ntnu.idatt2106.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import no.ntnu.idatt2106.exception.StatusCodeException;
+import no.ntnu.idatt2106.middleware.RequireAuth;
 import no.ntnu.idatt2106.model.DAO.CommunityDAO;
 import no.ntnu.idatt2106.model.DAO.UserDAO;
+import no.ntnu.idatt2106.model.DTO.TokenDTO;
 import no.ntnu.idatt2106.model.DTO.UserCommunityDTO;
 import no.ntnu.idatt2106.repository.CommunityRepository;
 import no.ntnu.idatt2106.service.UserCommunityService;
 import no.ntnu.idatt2106.service.UserService;
+import no.ntnu.idatt2106.util.TokenUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin
 @ApiResponse(responseCode = "200")
+@RequireAuth
 public class UserCommunityController {
     private final UserCommunityService userCommunityService;
     private final UserService userService;
@@ -35,8 +38,8 @@ public class UserCommunityController {
     @ApiResponse(responseCode = "400", description = "Illegal operation")
 
     public void addUserToCommunity(@PathVariable int communityId) throws StatusCodeException {
-        /*
-        UserDAO user = userService.findUserByUserId(userCommunityDTO.getUserID());
+        TokenDTO token = TokenUtil.getDataJWT();
+        UserDAO user = userService.findUserByUserId(Integer.parseInt(token.getAccountId()));
         if (user == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "User does not exist");
         }
@@ -44,24 +47,23 @@ public class UserCommunityController {
         if (communityDAO == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "Community does not exist");
         }
-        if(communityDAO.getVisibility()){
-            throw new StatusCodeException(HttpStatus.BAD_REQUEST, "This community is invite only")
+        if(communityDAO.getVisibility()==0){
+            throw new StatusCodeException(HttpStatus.BAD_REQUEST, "This community is invite only");
         }
         if (userCommunityService.userIsInCommunity(user,communityDAO)){
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "User is already in this community");
         }
         userCommunityService.addUserToCommunity(user, communityDAO);
 
-
-  */
     }
 
     @Operation(summary = "Get all communities a specific user is part of")
     @ApiResponse(responseCode = "200", description = "Found communities")
     @ApiResponse(responseCode = "400", description = "Illegal operation")
     @GetMapping("/user/communities")
-    public ArrayList<CommunityDAO> getCommunitiesForUser(@RequestBody UserCommunityDTO userCommunityDTO) throws StatusCodeException {
-        UserDAO user = userService.findUserByUserId(userCommunityDTO.getUserID());
+    public ArrayList<CommunityDAO> getCommunitiesForUser() throws StatusCodeException {
+        TokenDTO token = TokenUtil.getDataJWT();
+        UserDAO user = userService.findUserByUserId(Integer.parseInt(token.getAccountId()));
         if (user == null) {
             throw new StatusCodeException(HttpStatus.BAD_REQUEST, "User does not exist");
         }
