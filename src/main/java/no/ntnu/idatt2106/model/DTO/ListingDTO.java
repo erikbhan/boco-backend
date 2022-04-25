bd217633b0ed18ee7b5c4ba9d398c98d0134b0da
henrikburmann <haburmann@gmail.com>
2022-04-25 13:46:49 +0200

    Stash

---
+++ src/main/java/no/ntnu/idatt2106/model/DTO/ListingDTO.java
@@ -0,0 +1,51 @@
+package no.ntnu.idatt2106.model.DTO;
+
+public class ListingDTO {
+    private String title;
+    private String description;
+    private double pricePerDay;
+    private String address;
+    private long userID;
+
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPricePerDay() {
+        return this.pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getUserID() {
+        return this.userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+}
---
+++ src/main/java/no/ntnu/idatt2106/service/ListingService.java
@@ -0,0 +1,34 @@
+package no.ntnu.idatt2106.service;
+
+import no.ntnu.idatt2106.model.DAO.ListingDAO;
+import no.ntnu.idatt2106.model.DAO.UserDAO;
+import no.ntnu.idatt2106.model.DTO.ListingDTO;
+import no.ntnu.idatt2106.repository.ListingRepository;
+import no.ntnu.idatt2106.repository.UserRepository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ListingService {
+    private final ListingRepository listingRepository;
+    private final UserRepository userRepository;
+
+    public ListingService(ListingRepository listingRepository, UserRepository userRepository) {
+        this.listingRepository = listingRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void saveListing(ListingDTO listingDTO){
+        ListingDAO listing = new ListingDAO();
+        listing.setTitle(listingDTO.getTitle());
+        listing.setDescription(listingDTO.getAddress());
+        listing.setAddress(listingDTO.getAddress());
+        listing.setPricePerDay(listingDTO.getPricePerDay());
+        //listing.setUserID(userRepository.getById(listingDTO.getUserID()));
+        listingRepository.save(listing);
+    }
+
+    
+}
