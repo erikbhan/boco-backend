a7590f8b0e21a9dbf61348cbe7b5f9c4693d51a1
Håkon Røskaft <haakoero@stud.ntnu.no>
2022-04-27 10:02:56 +0200

    Made sql scripts some of the tests

---
+++ src/test/resources/cleanup_community.sql
@@ -0,0 +1,9 @@
+DELETE FROM public.community WHERE community_id = 4000;
+DELETE FROM public.community WHERE community_id = 4001;
+DELETE FROM public.community WHERE community_id = 4002;
+DELETE FROM public.community WHERE community_id = 4444;
+DELETE FROM public.community WHERE community_id = 1000;
+DELETE FROM public.community WHERE community_id = 1001;
+DELETE FROM public.community WHERE description = 'kul klubb';
+
+DELETE FROM public.user WHERE email = 'test@email.com';
---
+++ src/test/resources/data_community.sql
@@ -0,0 +1,20 @@
+INSERT INTO public.user (user_id, email, first_name, last_name, address, picture, salt, hash)
+VALUES (2022, 'test@email.com', 'test', 'user', 'gløshaugen', 'ok', 'l/hjdIHi9Us2uJZ7MP/urY6ALjISdukPrN5sjpD7wTMEV+DnQkWzOF3qfnO6r2PnIQM6zP7ZcdEYh0Gdok8nFQ==', 'Ge7Y9frKWdgKcAysHdYCIoOOsAcn9We3f2+C74xlc6kWQZn2scBE8sEf4iZezwsmG/KdeeEuspZD9Q4Ojt27Hg==');
+
+INSERT INTO public.community(community_id, name, description, visibility, location, picture)
+VALUES (4000, 'Einars MC klubb', 'vi som liker motorsykkel og heter Einar', 0, 'Elgeseter gate(midt på natta)', 'bilde');
+
+INSERT INTO public.community(community_id, name, description, visibility, location, picture)
+VALUES (4001, 'Haakon MC klubb', 'vi som liker motorsykkel og heter Haakon', 0, 'Elgeseter gate(midt på natta)', 'bilde');
+
+INSERT INTO public.community(community_id, name, description, visibility, location, picture)
+VALUES (4002, 'Sander MC klubb', 'vi som liker motorsykkel og heter Sander', 0, 'Elgeseter gate(midt på natta)', 'bilde');
+
+INSERT INTO public.community(community_id, name, description, visibility, location, picture)
+VALUES (4444, 'Aleks MC klubb', 'vi som liker motorsykkel og heter Aleks', 0, 'Elgeseter gate(midt på natta)', 'bilde');
+
+INSERT INTO public.community(community_id, description, location, name, picture, visibility)
+VALUES (1000, 'En hyggelig dag', 'Storvold', 'Vi som liker været', 'Nei takk', 1);
+
+INSERT INTO public.community(community_id, description, location, name, picture, visibility)
+VALUES (1001, 'Fisk for folk', 'Ravnkloa', 'Det regner fisk', 'imagen a place ...with fish', 1);
