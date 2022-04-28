62dc57d84732a7a387f9a5942a731e9254197123
Haakon Tideman Kanter <haakotka@stud.ntnu.no>
2022-04-28 15:38:30 +0200

    Fixed tests

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,2 +1,16 @@
-Drop schema public cascade;
-create schema public;
+DELETE FROM public.listing_category;
+DELETE FROM public.category;
+DELETE FROM public.chat_message;
+DELETE FROM public.comment;
+DELETE FROM public.community_listing;
+DELETE FROM public.user_community;
+DELETE FROM public.community;
+DELETE FROM public.community_request;
+DELETE FROM public.feed_entry;
+DELETE FROM public.feed_entry_picture;
+DELETE FROM public.rent;
+DELETE FROM public.listing;
+DELETE FROM public.listing_picture;
+DELETE FROM public.notification;
+DELETE FROM public.rating;
+DELETE FROM public.user;
